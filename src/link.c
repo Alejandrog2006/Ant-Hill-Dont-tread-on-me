d9a1ef57ccda50979d21a7484c24ab582ccf38ba
aleister12-9 <izan.robles@estudiante.uam.es>
2025-04-01 09:47:53 +0200

    Add files via upload

--- src/link.c
+++ src/link.c
@@ -2,7 +2,7 @@
  * @brief It implements the link module interface
  *
  * @file link.c
- * @author Rodrigo Cruz
+ * @author Rodrigo Cruz Asensio
  * @version 1.0
  * @date 20-03-2025
  * @copyright GNU Public License
@@ -10,148 +10,168 @@
 
 #include "link.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-struct _Link {
-    Id id; /* Link ID */
-    char name[WORD_SIZE + 1]; /* Link name */
-    Id origin; /* Origin ID */
-    Id destination; /* Destination ID */
-    Direction direction; /* Direction of the link */
-    Bool open; /* Link status (open or closed) */
+struct _Link
+{
+  Id id;
+  char name[WORD_SIZE + 1];
+  Id origin;
+  Id destination;
+  Direction direction;
+  Bool open;
 };
 
-Link* link_create(Id id) {
-    Link* newLink = NULL;
+Link *link_create(Id id)
+{
+  Link *newLink = NULL;
+  if (id == NO_ID)
+    return NULL; /* Error control */
 
-    if (id == NO_ID) {
-        return NULL;
-    }
+  newLink = (Link *)malloc(sizeof(Link)); /* Memory allocation */
 
-    newLink = (Link*)malloc(sizeof(Link));
-    if (newLink == NULL) {
-        return NULL;
-    }
+  if (newLink == NULL)
+  {
+    return NULL; /* Error control */
+  }
 
-    newLink->id = id;
-    newLink->name[0] = '\0';
-    newLink->origin = NO_ID;
-    newLink->destination = NO_ID;
-    newLink->direction = NONE;
-    newLink->open = TRUE;
+  newLink->id = id; /* Initialization of the structure */
+  newLink->name[0] = '\0';
+  newLink->origin = NO_ID;
+  newLink->destination = NO_ID;
+  newLink->direction = NONE;
+  newLink->open = TRUE;
 
-    return newLink;
+  return newLink;
 }
 
-Status link_destroy(Link* link) {
-    if (!link) {
-        return ERROR;
-    }
+Status link_destroy(Link *link)
+{
+  if (!link)
+    return ERROR;
 
-    free(link);
-    return OK;
+  free(link);
+
+  return OK;
 }
 
-Status link_set_name(Link* link, char* name) {
-    if (!link || !name) {
-        return ERROR;
-    }
+Status link_set_name(Link *link, char *name)
+{
+  if (!link || !name)
+    return ERROR;
 
-    if (!strcpy(link->name, name)) {
-        return ERROR;
-    }
+  if (!strcpy(link->name, name))
+  {
+    return ERROR;
+  }
 
-    return OK;
+  return OK;
 }
 
-const char* link_get_name(Link* link) {
-    if (!link) {
-        return NULL;
-    }
+const char *link_get_name(Link *link)
+{
+  if (!link)
+  {
+    return NULL;
+  }
 
-    return link->name;
+  return link->name;
 }
 
-Status link_set_origin(Link* link, Id origin) {
-    if (!link || origin == NO_ID) {
-        return ERROR;
-    }
+Status link_set_origin(Link *link, Id origin)
+{
+  if (!link || origin == NO_ID)
+  {
+    return ERROR;
+  }
 
-    link->origin = origin;
-    return OK;
+  link->origin = origin;
+  return OK;
 }
 
-Id link_get_origin(Link* link) {
-    if (!link) {
-        return NO_ID;
-    }
+Id link_get_origin(Link *link)
+{
+  if (!link)
+  {
+    return NO_ID;
+  }
 
-    return link->origin;
+  return link->origin;
 }
 
-Status link_set_destination(Link* link, Id destination) {
-    if (!link || destination == NO_ID) {
-        return ERROR;
-    }
+Status link_set_destination(Link *link, Id destination)
+{
+  if (!link || destination == NO_ID)
+  {
+    return ERROR;
+  }
 
-    link->destination = destination;
-    return OK;
+  link->destination = destination;
+  return OK;
 }
 
-Id link_get_destination(Link* link) {
-    if (!link) {
-        return NO_ID;
-    }
+Id link_get_destination(Link *link)
+{
+  if (!link)
+  {
+    return NO_ID;
+  }
 
-    return link->destination;
+  return link->destination;
 }
 
-Status link_set_direction(Link* link, Direction direction) {
-    if (!link || direction == NONE) {
-        return ERROR;
-    }
+Status link_set_direction(Link *link, Direction direction)
+{
+  if (!link || direction == NONE)
+  {
+    return ERROR;
+  }
 
-    link->direction = direction;
-    return OK;
+  link->direction = direction;
+  return OK;
 }
 
-Direction link_get_direction(Link* link) {
-    if (!link) {
-        return NONE;
-    }
+Direction link_get_direction(Link *link)
+{
+  if (!link)
+  {
+    return NONE;
+  }
 
-    return link->direction;
+  return link->direction;
 }
 
-Status link_set_open(Link* link, Bool open) {
-    if (!link) {
-        return ERROR;
-    }
+Status link_set_open(Link *link, Bool open)
+{
+  if (!link || open == FALSE)
+  {
+    return ERROR;
+  }
 
-    link->open = open;
-    return OK;
+  link->open = open;
+  return OK;
 }
 
-Bool link_get_open(Link* link) {
-    if (!link) {
-        return FALSE;
-    }
+Bool link_get_open(Link *link)
+{
+  if (!link)
+  {
+    return FALSE;
+  }
 
-    return link->open;
+  return link->open;
 }
 
-void link_print(Link* link) {
-    if (!link) {
-        printf("Link ERROR\n");
-        return;
-    }
+void link_print(Link *link)
+{
+  if (!link)
+  {
+    printf("Link ERROR\n");
+    return;
+  }
 
-    printf("Link ID: %d\n", link->id);
-    printf("Link name: %s\n", link->name);
-    printf("Link origin: %d\n", link->origin);
-    printf("Link destination: %d\n", link->destination);
-    printf("Link direction: %d\n", link->direction);
-    printf("¿Is the Link open?: %s\n", link->open ? "Yes" : "No");
+  printf("Link ID: %ld\n", link->id);
+  printf("Link name: %s\n", link->name);
+  printf("Link origin: %ld\n", link->origin);
+  printf("Link destination: %ld\n", link->destination);
+  printf("Link direction: %d\n", link->direction);
+  printf("¿Is the Link open?: %s\n", link->open ? "Yes" : "No");
 }
