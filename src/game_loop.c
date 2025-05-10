4684d480d54ab9c628bab68fd87c91ab5f152c5e
aleister12-9 <izan.robles@estudiante.uam.es>
2025-05-10 10:21:55 +0200

    Add files via upload

--- src/game_loop.c
+++ src/game_loop.c
@@ -308,6 +308,10 @@ void game_loop_log(Game *game, FILE *f, Command *last_cmd, Status status)
             fprintf(f, "Player %d: LOAD - %s\n", game_get_turn(game) + 1, char_stat);
             break;
 
+        case PASS:
+            fprintf(f, "Player %d: PASS - %s\n", game_get_turn(game) + 1, char_stat);
+            break;
+
         default:
             break;
         }
